4a1db040aa6d11158a084bec26336736cdb87b86
ahmadragheb <ahmedragheb75@gmail.com>
2020-05-11 00:19:58 +0300

    fix api

--- barcode_printer/api.py
+++ barcode_printer/api.py
@@ -2,6 +2,9 @@ import frappe
 
 
 def create_barcode(doc, method):
-    if doc.barcode_label != doc.name:
-        doc.barcode_label = doc.name
-        doc.save()
+    if doc.docstatus == 1:
+        return 
+    else:
+        if doc.barcode_label != doc.name:
+            doc.barcode_label = doc.name
+            doc.save()
